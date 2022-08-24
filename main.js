361b576f5f2d75ee0a09f2a73998d2b7e5553b70
suuu0122 <suuu0122lab@gmail.com>
2022-08-24 23:12:33 +0900

    add resetScreen

--- main.js
+++ main.js
@@ -102,7 +102,7 @@ function sleep(waitSec, callbackFunc) {
     }, 1000);
 }
 
-function to_result_screen(){
+function toResultScreen(){
     let init = document.getElementById('init');
     let loading = document.getElementById('loading');
     let result = document.getElementById('result');
@@ -117,15 +117,21 @@ function to_result_screen(){
     })
 }
 
-function to_initial_screen(){
+function toInitialScreen(){
+	resetScreen();
     let init = document.getElementById('init');
     let result = document.getElementById('result');
     init.classList.remove("d-none");
     result.classList.add("d-none")
 }
 
-let start_button = document.getElementById('start');
-start_button.onclick = to_result_screen;
+function resetScreen() {
+	let resultScreen = document.getElementById("result");
+	resultScreen.scrollTop = 0;
+}
 
-let restart_button = document.getElementById('restart');
-restart_button.onclick = to_initial_screen;
+let startButton = document.getElementById('start');
+startButton.onclick = toResultScreen;
+
+let restartButton = document.getElementById('restart');
+restartButton.onclick = toInitialScreen;
